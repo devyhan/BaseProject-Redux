f07eb34d059211db271f93d77f078e4d78b87767
Yhansol <devyhan@you-sol.com>
2021-06-11 10:24:36 +0900

    Initial Commit

---
+++ Shared/BaseProject_ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseProject_ReduxApp.swift
+//  Shared
+//
+//  Created by 조요한 on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct BaseProject_ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
